ec72024e608571f5d67c94d8176ce05921a5a5d2
Brightness999 <brightness0804@gmail.com>
2020-11-02 09:52:15 -0700

    stream builder for the current screen

--- mobile_app/lib/screens/community-board/board/board_ui.dart
+++ mobile_app/lib/screens/community-board/board/board_ui.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 class BoardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Community Board'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Board Screen'),
+      ),
+      body: Center(
+        child: Text('Community Board'),
+      ),
     );
   }
 }
