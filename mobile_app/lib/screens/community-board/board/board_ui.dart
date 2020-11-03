f8433643c9c69a9191569621b99770b57979f685
Brightness999 <brightness0804@gmail.com>
2020-11-02 19:14:10 -0700

    login screen added as first view

--- mobile_app/lib/screens/community-board/board/board_ui.dart
+++ mobile_app/lib/screens/community-board/board/board_ui.dart
@@ -6,12 +6,12 @@ class BoardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Community'),
+        title: Text('Community Screen'),
         actions: [
           IconButton(
             icon: Icon(Icons.logout),
             onPressed: () {
-              Navigator.push(
+              Navigator.pushReplacement(
                 context,
                 MaterialPageRoute(builder: (context) {
                   return LoginScreen();
