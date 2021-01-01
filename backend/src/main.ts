823524dee0bac1b0a01e7ff817fbf8362b0d1c39
faridg18 <farid@parabeac.com>
2021-01-01 10:50:44 -0700

    asdf

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,17 +20,18 @@ async function bootstrap() {
   );
 
   app.enableCors({
-    origin: (origin, callback) => {
-      if (whitelist.includes(origin)) return callback(null, true);
-
-      callback(new Error('Not allowed by CORS'));
-    },
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    origin: true,
     methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
     credentials: true,
   });
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+  
   app.setGlobalPrefix('api');
 
   const options = new DocumentBuilder().build();
