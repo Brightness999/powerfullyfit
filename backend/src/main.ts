2534c85b89a852857976dbe6cd5b955817b9f3c1
Brightness999 <brightness0804@gmail.com>
2021-01-01 10:54:29 -0700

    asdf

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -26,12 +26,12 @@ async function bootstrap() {
   });
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
   });
-  
+
   app.setGlobalPrefix('api');
 
   const options = new DocumentBuilder().build();
