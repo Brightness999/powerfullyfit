e5612df70918b1d268377d5fe93adb4b7407f2a8
Brightness999 <brightness0804@gmail.com>
2021-01-07 09:30:49 -0700

    canged program dto

--- backend/src/program/dto/create-program.dto.ts
+++ backend/src/program/dto/create-program.dto.ts
@@ -1,6 +1,12 @@
-import { IsString } from 'class-validator';
+import { IsString, IsInt } from 'class-validator';
 
 export class CreateProgramDto {
   @IsString()
   name: string;
+
+  @IsInt()
+  weeks: number;
+
+  @IsInt()
+  phases: number;
 }
