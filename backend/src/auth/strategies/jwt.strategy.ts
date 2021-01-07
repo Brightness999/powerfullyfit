f987e07e5f456ffd711df194fc294958ee443e58
Brightness999 <brightness0804@gmail.com>
2021-01-07 10:39:12 -0700

    asdf

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -14,6 +14,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
+    console.log(payload);
+    
     return { userId: payload.sub, username: payload.username };
   }
 }
